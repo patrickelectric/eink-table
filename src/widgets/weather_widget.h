ff306a66ff248e0efac2fbe7d61dcbf7cadfe53c
Patrick José Pereira <patrickelectric@gmail.com>
2025-03-30 09:11:42 -0300

    widgets: wather: Remove error message
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/widgets/weather_widget.h
+++ src/widgets/weather_widget.h
@@ -34,50 +34,52 @@ public:
 
         Request request("https://wttr.in/Florianopolis?format=j1");
 
-        if (request.isSuccessful()) {
-            JsonDocument doc;
-            deserializeJson(doc, request.getResponse());
-
-            const char* tempC = doc["current_condition"][0]["temp_C"];
-            const char* desc = doc["current_condition"][0]["weatherDesc"][0]["value"];
-
-            String icon_txt;
-            String desc_str(desc);
-            desc_str.toLowerCase();
-
-            if (desc_str.indexOf("partly") >= 0 && desc_str.indexOf("cloud") >= 0) {
-                icon_txt = "\uf6c4";
-            } else if (desc_str.indexOf("sun") >= 0) {
-                icon_txt = "\uf185";
-            } else if (desc_str.indexOf("cloud") >= 0) {
-                icon_txt = "\uf0c2";
-            } else if (desc_str.indexOf("rain") >= 0) {
-                icon_txt = "\uf73d";
-            } else if (desc_str.indexOf("storm") >= 0 || desc_str.indexOf("thunder") >= 0) {
-                icon_txt = "\uf0e7";
-            } else if (desc_str.indexOf("snow") >= 0) {
-                icon_txt = "\uf2dc";
-            } else if (desc_str.indexOf("fog") >= 0 || desc_str.indexOf("mist") >= 0) {
-                icon_txt = "\uf75f";
-            } else if (desc_str.indexOf("clear") >= 0) {
-                icon_txt = "\uf186";
-            } else {
-                icon_txt = String("\uf128 ") + desc_str;
-            }
-
-            static lv_obj_t* icon = lv_label_create(cont);
-            lv_label_set_text(icon, icon_txt.c_str());
-            lv_obj_set_style_text_font(icon, &font_awesome, 0);
-
-            static lv_obj_t* label = lv_label_create(cont);
-            String weather_info = String(desc) + " " + tempC + "°C";
-            lv_label_set_text(label, weather_info.c_str());
-            return true;
-        } else {
-            static lv_obj_t* label = lv_label_create(cont);
-            lv_label_set_text(label, "Weather fetch error");
+        static lv_obj_t* bad_label = lv_label_create(cont);
+        if (!request.isSuccessful()) {
+            static lv_obj_t* bad_label = lv_label_create(cont);
+            lv_label_set_text(bad_label, "Weather fetch error");
             return false;
         }
+        lv_label_set_text(bad_label, "");
+
+        JsonDocument doc;
+        deserializeJson(doc, request.getResponse());
+
+        const char* tempC = doc["current_condition"][0]["temp_C"];
+        const char* desc = doc["current_condition"][0]["weatherDesc"][0]["value"];
+
+        String icon_txt;
+        String desc_str(desc);
+        desc_str.toLowerCase();
+
+        if (desc_str.indexOf("partly") >= 0 && desc_str.indexOf("cloud") >= 0) {
+            icon_txt = "\uf6c4";
+        } else if (desc_str.indexOf("sun") >= 0) {
+            icon_txt = "\uf185";
+        } else if (desc_str.indexOf("cloud") >= 0) {
+            icon_txt = "\uf0c2";
+        } else if (desc_str.indexOf("rain") >= 0) {
+            icon_txt = "\uf73d";
+        } else if (desc_str.indexOf("storm") >= 0 || desc_str.indexOf("thunder") >= 0) {
+            icon_txt = "\uf0e7";
+        } else if (desc_str.indexOf("snow") >= 0) {
+            icon_txt = "\uf2dc";
+        } else if (desc_str.indexOf("fog") >= 0 || desc_str.indexOf("mist") >= 0) {
+            icon_txt = "\uf75f";
+        } else if (desc_str.indexOf("clear") >= 0) {
+            icon_txt = "\uf186";
+        } else {
+            icon_txt = String("\uf128 ") + desc_str;
+        }
+
+        static lv_obj_t* icon = lv_label_create(cont);
+        lv_label_set_text(icon, icon_txt.c_str());
+        lv_obj_set_style_text_font(icon, &font_awesome, 0);
+
+        static lv_obj_t* good_label = lv_label_create(cont);
+        String weather_info = String(desc) + " " + tempC + "°C";
+        lv_label_set_text(good_label, weather_info.c_str());
+        return true;
     }
 
     void update()
