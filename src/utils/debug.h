817291c837bbd91b764a804fd9f27ee865df3f38
Patrick José Pereira <patrickelectric@gmail.com>
2025-03-31 22:20:01 -0300

    utils: debug: Use new WebServerManager log
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -1,16 +1,18 @@
 #pragma once
 
+#include "utils/server.h"
+
 #define DEBUG
 
 #ifdef DEBUG
 #define debug(fmt, args...)                                                      \
     do {                                                                         \
         char buffer[256];                                                        \
-        snprintf(buffer, sizeof(buffer), "[%s:%d]: " fmt "\n\r",                 \
-            __PRETTY_FUNCTION__, __LINE__, ##args);                              \
-        Serial.print(buffer);                                                    \
         snprintf(buffer, sizeof(buffer), "[%s:%d]: " fmt "\n",                   \
             __PRETTY_FUNCTION__, __LINE__, ##args);                              \
+        Serial.print(buffer);                                                    \
+        Serial.print('\r');                                                      \
+        WebServerManager::self()->log(buffer);                                   \
     } while (0)
 #else
 #define debug(fmt, args...)
